fcd31601d546f655cbbc49e91d7a3eac7c8293a0
Alf21 <4lf-mueller@gmx.de>
2018-11-23 15:42:53 +0100

    Fix for model animation

--- gamemodes/terrortown/entities/entities/vampire/pigeon.lua
+++ gamemodes/terrortown/entities/entities/vampire/pigeon.lua
@@ -215,7 +215,7 @@ function PIGEON.Hooks.SetModel(player)
 end
 
 function PIGEON.Hooks.UpdateAnimation(player)
-	if not player.pigeon then return end
+	if not player.pigeon or PIGEON.model ~= "models/pigeon.mdl" then return end
 
 	local rate = 2
 	local sequence = "idle01"
