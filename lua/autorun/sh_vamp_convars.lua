8cdc192c8da66a20f253504773f0b08bfe963bcd
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-07-31 22:47:06 +0200

    tab fix #2

--- lua/autorun/sh_vamp_convars.lua
+++ lua/autorun/sh_vamp_convars.lua
@@ -1,21 +1,21 @@
-CreateConVar("ttt2_vamp_bloodtime", "60", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-CreateConVar("ttt2_vamp_maxhealth", "250", {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+CreateConVar("ttt2_vamp_bloodtime", "60",   {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+CreateConVar("ttt2_vamp_maxhealth", "250",  {FCVAR_ARCHIVE, FCVAR_NOTIFY})
 
-hook.Add("TTTUlxDynamicRCVars", "TTTUlxDynamicVampCVars", function(tbl)
-	tbl[ROLE_VAMPIRE] = tbl[ROLE_VAMPIRE] or {}
+hook.Add("TTTUlxDynamicRCVars", "TTTUlxDynamicVampCVars",   function(tbl)
+	tbl[ROLE_VAMPIRE]   =   tbl[ROLE_VAMPIRE]   or  {}
 
 	table.insert(tbl[ROLE_VAMPIRE], {
-        cvar = "ttt2_vamp_bloodtime", 
-        slider = true, 
-        desc = "vampire bloodlust time (def: 60)"
+        cvar    =   "ttt2_vamp_bloodtime", 
+        slider  =   true, 
+        desc    =   "vampire bloodlust time (def: 60)"
     })
 
     table.insert(tbl[ROLE_VAMPIRE], {
-        cvar = "ttt2_vamp_maxhealth", 
-        slider = true, 
-        min = 100,
-        max = 600, 
-        desc = "vampire max health (def: 250)"
+        cvar    =   "ttt2_vamp_maxhealth", 
+        slider  =   true, 
+        min =   100,
+        max =   600, 
+        desc    =   "vampire max health (def: 250)"
     })
 
 end)
