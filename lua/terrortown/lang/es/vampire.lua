0048821978af880510ea303b0d59307b1b3e4c65
mexikoedi <mexikoedi@outlook.com>
2023-10-08 21:18:44 +0200

    moved convars to F1 menu (#13)

--- lua/terrortown/lang/es/vampire.lua
+++ lua/terrortown/lang/es/vampire.lua
@@ -10,3 +10,7 @@ L["search_role_" .. VAMPIRE.abbr] = "Esta persona era un Vampiro."
 L["target_" .. VAMPIRE.name] = "Vampiro"
 L["ttt2_desc_" .. VAMPIRE.name] = [[El Vampiro es un traidor que no puede acceder a la tienda ([C]), pero puede transformarse en murciélago/ave con [LALT] (la tecla de caminar). Para balancearlo, el Vampiro necesita matar a una persona por minuto. De lo contrario, caerá en el estado de Lujuria de Sangre. En este estado, el Vampiro pierde vida por segundo.
 Además, en ese estado se cura un 50% del daño que le haga a otras personas. Si así lo desea, al estar en Lujuria de Sangre, puede transformarse en un murciélago/ave. Puedes activar este estado transformándote voluntariamente, aunque no podrás deshacerlo.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+--L["label_vamp_bloodtime"] = "Vampire bloodlust time"
+--L["label_vamp_maxhealth"] = "Vampire max health"
